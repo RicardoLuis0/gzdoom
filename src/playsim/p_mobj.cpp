bf046589fe461a1a3b87c016a01f5193d5d052c6
Ricardo Luís Vaz Silva <ricolvs123@gmail.com>
2025-01-29 01:44:53 -0300

    improve bone recalculation logic

--- src/playsim/p_mobj.cpp
+++ src/playsim/p_mobj.cpp
@@ -592,6 +592,8 @@ bool AActor::SetState (FState *newstate, bool nofunction)
 			}
 		}
 
+		CalcBones(true);
+
 		if (!nofunction)
 		{
 			FState *returned_state;
@@ -619,6 +621,7 @@ bool AActor::SetState (FState *newstate, bool nofunction)
 		flags8 |= MF8_RECREATELIGHTS;
 		Level->flags3 |= LEVEL3_LIGHTCREATED;
 	}
+
 	return true;
 }
 
@@ -3923,12 +3926,15 @@ void AActor::Tick ()
 			return;
 	}
 
-	CalcBones(false);
-
 	if (freezetics > 0)
 	{
 		freezetics--;
 		return;
+	}
+
+	if(flags9 & MF9_DECOUPLEDANIMATIONS)
+	{
+		CalcBones(false);
 	}
 
 	AActor *onmo;
@@ -4485,6 +4491,11 @@ void AActor::Tick ()
 			if (!SetState(state->GetNextState()))
 				return; 		// freed itself
 		}
+	}
+
+	if(!(flags9 & MF9_DECOUPLEDANIMATIONS) && modelData && !(modelData->flags & MODELDATA_GET_BONE_INFO_RECALC))
+	{
+		CalcBones(false);
 	}
 
 	if (tics == -1 || state->GetCanRaise())
